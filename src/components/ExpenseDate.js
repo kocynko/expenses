132e1ac4af37fe344159b6f0fe6f93ea4e3faf95
MatusKocian00 <xkocian@stuba.sk>
2022-11-13 13:25:25 +0100

    initial commit

---
+++ src/components/ExpenseDate.js
@@ -0,0 +1,15 @@
+import "./ExpenseDate.css";
+import Card from "./Card";
+function ExpenseDate(props) {
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
+  return (
+    <Card className="expense-item__date">
+      <div className="expense-item__date_day">{day}</div>
+      <div className="expense-item__date_month">{month}</div>
+      <div className="expense-item__date_year">{year}</div>
+    </Card>
+  );
+}
+export default ExpenseDate;
