f073dd21176df7170e6608b3d636cfa20db248ef
MatusKocian00 <xkocian@stuba.sk>
2022-11-13 22:08:37 +0100

    click handler

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,6 +3,9 @@ import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
+  const clickHandler = () => {
+    
+  }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -10,6 +13,7 @@ function ExpenseItem(props) {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount.toFixed(2)}</div>
       </div>
+      <button onClick={}>Change Title</button>
     </Card>
   );
 }
