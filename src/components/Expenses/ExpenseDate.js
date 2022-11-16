1103829753487458a7dff8b547a2dac0acb72752
MatusKocian00 <xkocian@stuba.sk>
2022-11-16 11:53:57 +0100

    'new-expense form'

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -1,16 +1,15 @@
-import "./ExpenseDate.css";
-import Card from "../UI/Card";
+import './ExpenseDate.css';
 
 function ExpenseDate(props) {
-  const month = props.date.toLocaleString("en-US", { month: "long" });
-  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const month = props.date.toLocaleString('en-US', { month: 'long' });
+  const day = props.date.toLocaleString('en-US', { day: '2-digit' });
   const year = props.date.getFullYear();
   return (
-    <Card className="expense-item__date">
+    <div className="expense-item__date">
       <div className="expense-item__date_day">{day}</div>
       <div className="expense-item__date_month">{month}</div>
       <div className="expense-item__date_year">{year}</div>
-    </Card>
+    </div>
   );
 }
 export default ExpenseDate;
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,27 @@
-import ExpenseDate from "./ExpenseDate";
-import Card from "../UI/Card";
-import "./ExpenseItem.css";
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+import './ExpenseItem.css';
+import { useState } from 'react';
 
 function ExpenseItem(props) {
+  const [counter, setCounter] = useState(1);
+  const [title, setTitle] = useState(props.title);
+  const [date, setDate] = useState(props.date);
+  const [amount, setAmount] = useState(props.amount);
+
   const clickHandler = () => {
-    console.log("click");
+    setAmount(20 * counter);
+    setDate(new Date());
+    setTitle(`Updated for ${counter} times`);
+    setCounter(counter + 1);
   };
+
   return (
     <Card className="expense-item">
-      <ExpenseDate date={props.date} />
+      <ExpenseDate date={date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">${props.amount.toFixed(2)}</div>
+        <h2>{title}</h2>
+        <div className="expense-item__price">${amount.toFixed(2)}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
     </Card>
